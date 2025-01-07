a6c11c3cd594f2ff8f181abad51b5fefb41401d2
kyokosawada <gcpaps2@gmail.com>
2025-01-07 08:48:04 +0800

    Groups

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -10,6 +11,16 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_HEIGHT / 2, SCREEN_HEIGHT / 2)
+
     game_running = True
     while game_running:
         for event in pygame.event.get():
@@ -17,7 +28,16 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
